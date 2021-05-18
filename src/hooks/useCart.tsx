f0b41bc7263e118f09386129b7cfe5578e8cb187
Jhonata <gutemberg.jhonata@gmail.com>
2021-05-18 10:22:54 -0300

    Implement feacture AddProduct

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { createContext, ReactNode, useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,20 +23,51 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const savedProductIndex = cart.findIndex(
+        (product) => product.id === productId
+      );
+
+      if (savedProductIndex >= 0) {
+        const savedProduct = cart[savedProductIndex];
+
+        const { data } = await api.get(`stock/${productId}`);
+        const stockAmount = await data.amount;
+
+        if (savedProduct.amount < stockAmount) {
+          savedProduct.amount++;
+
+          const updatedCart = [
+            ...cart.slice(0, savedProductIndex),
+            savedProduct,
+            ...cart.slice(savedProductIndex + 1, cart.length),
+          ];
+          setCart(updatedCart);
+          localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+        } else {
+          toast.error('Quantidade solicitada fora de estoque');
+        }
+      } else {
+        const { data } = await api.get(`products/${productId}`);
+        data.amount = 1;
+        
+        const updatedCart = [...cart, data];
+        setCart(updatedCart);
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart)); 
+      }
+
     } catch {
-      // TODO
+      toast.error("Erro na adição do produto");
     }
   };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { MdAddShoppingCart } from "react-icons/md";
 
 import { ProductList } from "./styles";
@@ -23,22 +23,21 @@ interface CartItemsAmount {
 
 const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    return {
+      ...sumAmount,
+      [product.id]: product.amount,
+    };
+  }, {} as CartItemsAmount);
 
   useEffect(() => {
     async function loadProducts() {
       const response = await api.get("/products");
       const formatedProducts = response.data.map(
         (product: ProductFormatted) => {
-          product.priceFormatted = new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          }).format(product.price);
-
+          product.priceFormatted = formatPrice(product.price);
           return product;
         }
       );
@@ -50,7 +49,7 @@ const Home = (): JSX.Element => {
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    addProduct(id);
   }
 
   return (
@@ -58,20 +57,17 @@ const Home = (): JSX.Element => {
       {products.map((product) => {
         return (
           <li key={product.id}>
-            <img
-              src={product.image}
-              alt={product.title}
-            />
+            <img src={product.image} alt={product.title} />
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
             <button
               type="button"
               data-testid="add-product-button"
-              // onClick={() => handleAddProduct(product.id)}
+              onClick={() => handleAddProduct(product.id)}
             >
               <div data-testid="cart-product-quantity">
                 <MdAddShoppingCart size={16} color="#FFF" />
-                {/* {cartItemsAmount[product.id] || 0} */} 2
+                {cartItemsAmount[product.id] || 0}
               </div>
 
               <span>ADICIONAR AO CARRINHO</span>
